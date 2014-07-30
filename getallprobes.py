8f8f987bd28844ffd389bb2fe7eb459a8729651d
Marcin Mazurek <m.mazurek@netsync.pl>
2014-07-30 21:49:55 +0200

    Grabs all probes information from atlas.ripe.net that are late used to process data.

---
+++ getallprobes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+##
+## Grabs all probes information from atlas.ripe.net that are late used to process data.
+##
+
+import json
+import urllib2
+from pprint import pprint 
+
+all_probes_url = 'https://atlas.ripe.net/api/v1/probe/?limit=0'
+
+url_fh = urllib2.urlopen( all_probes_url )
+prb_info = json.load( url_fh )
+url_fh.close()
+
+for prb_id in prb_info['objects']:
+   print "%s %0.4f %0.4f %s %s %s" % ( prb_id['id'], prb_id['latitude'], prb_id['longitude'], prb_id['asn_v4'], prb_id['asn_v6'], prb_id['country_code'] ) 
+
